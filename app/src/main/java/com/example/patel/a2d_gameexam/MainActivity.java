4f40be2546d130414522d0c20b95f25c74c77f0b
Pra9_ptl <patelpranav1313@gmail.com>
2019-06-06 08:59:25 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/patel/a2d_gameexam/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.patel.a2d_gameexam;
+
+import android.graphics.Point;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+
+public class MainActivity extends AppCompatActivity {
+
+    GameEngine game;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get size of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        // Initialize the GameEngine object
+        // Pass it the screen size (height & width)
+        game = new GameEngine(this, size.x, size.y);
+
+        // Make GameEngine the view of the Activity
+        setContentView(game);
+    }
+
+    // Android Lifecycle functions
+    // ----------------------------
+
+    // This function gets run when user switches from the game to some other app on the phone
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Pause the game
+        game.pauseGame();
+    }
+
+    // This function gets run when user comes back to the game
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Start the game
+        game.startGame();
+    }
+
+}
